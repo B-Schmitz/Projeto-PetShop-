0eeb536af201746a3e75cf924263e584d5b6b2aa
comp8 <comp8@comp8_22.lab.unesc.net>
2017-03-31 21:31:00 -0300

    desabilitei a ediçao do JTable

--- ControlePetShop/src/Janelas/ControleEstoque.java
+++ ControlePetShop/src/Janelas/ControleEstoque.java
@@ -72,9 +72,16 @@ public class ControleEstoque extends javax.swing.JInternalFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         Table_Mostra_info.setToolTipText("");
@@ -243,8 +250,11 @@ public class ControleEstoque extends javax.swing.JInternalFrame {
 
     public JTable getTabela() {
         limpa();
+       
         return Table_Mostra_info;
+        
     }
+    
 
      public void limpa(){
         TextField_Fornecedor.setText(null);
